2890951cb9bddcac19e9d0da4b51abc289b32f55
shubhhaamm07 <shubhamrana009988@gmail.com>
2026-04-22 02:55:42 +0530

    Added CI/CD pipeline with Docker

--- Testing/Backend/app.py
+++ Testing/Backend/app.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# ✅ ADD THIS
+@app.route("/")
+def home():
+    return "Backend is running 🚀"
+
 @app.route("/student", methods=["POST"])
 def create_student():
     data = request.get_json()
@@ -10,3 +15,8 @@ def create_student():
         return jsonify({"error": "Name required"}), 400
 
     return jsonify({"message": "Student created"}), 201
+
+
+# OPTIONAL (but good practice for Docker)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
